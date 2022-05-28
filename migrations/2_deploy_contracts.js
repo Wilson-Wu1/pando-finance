fbedd451ac6b106b2464f214da89050f8a3d574b
Albert Vo <100101844+albert-vo-crypto@users.noreply.github.com>
2022-05-27 18:26:37 -0700

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,5 @@
 const NFTCollection = artifacts.require("NFTCollection");
 const NFTMarketplace = artifacts.require("NFTMarketplace");
-const RBFVault = artifacts.require("RBFVault");
 
 module.exports = async function (deployer) {
   await deployer.deploy(NFTCollection);
@@ -13,15 +12,8 @@ module.exports = async function (deployer) {
 
 
 
-const RBFVault = artifacts.require("RBFVault");
-const PaymentSplitter = artifacts.require("PaymentSplitter");
-const CompoundEth = artifacts.require("CompoundEth");
-const SendEther = artifacts.require("SendEther");
+
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
-  deployer.deploy(RBFVault);
-  deployer.deploy(PaymentSplitter);
-  deployer.deploy(CompoundEth);
-  deployer.deploy(SendEther);
 };
